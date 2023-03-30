f83798812b3feefa858da3593ff061a1faacdd47
Onesmus Wambugu <onesmus1024@gmail.com>
2023-03-30 14:52:55 +0300

    webpack & babel configuration

--- 03/demos/demos/before/src/index.js
+++ 03/demos/demos/before/src/index.js
@@ -5,10 +5,13 @@ import { render } from 'react-dom';
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello World test </h1>
     </div>
   );
 }
 
+
+
 render(<App />, document.getElementById('app'));
+
 
--- 03/demos/demos/before/webpack.config.dev.js
+++ 03/demos/demos/before/webpack.config.dev.js
@@ -3,39 +3,39 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    devtool: 'cheap-module-eval-source-map',
-    mode : 'development',
-    target : 'web',
+  devtool: "source-map",
+  mode: "development", // "production" | "development" | "none"
+  target: "web", // webpack-dev-server is a little faster with this
 
-    entry: [
-        'webpack-hot-middleware/client',
-        './src/index.js'
+  entry: ["./src/index"],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico",
+    }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("development"),
+    }),
+  ],
+
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
     ],
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/'
-    },
-    devServer: {
-        stats : 'minimal',
-        overlay : true,
-        historyApiFallback : true,
-        disableHostCheck : true,
-        headers : { 'Access-Control-Allow-Origin' : '*' },
-        https : false
-
-    },
-    Plugins : [
-        new HtmlWebpackPlugin({
-            template : 'src/index.html',
-            favicon : 'src/favicon.ico'
-        }),
-    ],
-
-    module: {
-        rules: [
-            { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] },
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
-        ]
-    }
+  },
 };
